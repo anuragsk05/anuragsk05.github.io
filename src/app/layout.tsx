80bdb763b9464ee171293cf00240d0f8eb79f0e9
anuragsk05 <157761197+anuragsk05@users.noreply.github.com>
2025-08-25 00:35:01 -0400

    Adjust icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,16 @@ export const metadata: Metadata = {
   description: 'Software Engineer • Musician • Creator',
   icons: {
     icon: [
-      { url: '/favicon.ico' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
+    shortcut: ['/favicon-32x32.png'],
     apple: { url: '/favicon-32x32.png' },
+    other: [
+      {
+        rel: 'icon',
+        url: '/favicon-32x32.png',
+      },
+    ],
   },
 };
 
@@ -25,7 +31,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="icon" href="/favicon-32x32.png" />
+        <link rel="shortcut icon" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="apple-touch-icon" href="/favicon-32x32.png" />
       </head>
       <body className={spaceGrotesk.className}>
